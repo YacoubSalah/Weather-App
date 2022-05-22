a460d27c31a5b3db76c05e7127e6779256d0566a
YacoubSalah <eng.yacoub-salah-89@hotmail.com>
2022-05-23 01:21:54 +0300

    heroku horray

--- server.js
+++ server.js
@@ -9,8 +9,12 @@ app.use(express.static(path.join(__dirname , "dist")))
 app.use(express.static(path.join(__dirname , "node_modules")))
 app.use("/", clientAPI)
 
-const port = 3000
-app.listen(port, function () { console.log(`App is listening on port: ${port}`) })
+/* const port = 3000
+app.listen(port, function () { console.log(`App is listening on port: ${port}`) }) */
+
+const PORT = 8080
+app.listen(process.env.PORT || PORT);
 
 const mongoose = require("mongoose")
-mongoose.connect("mongodb://localhost/Weather")
+mongoose.connect(process.env.MONGODB_URL||'mongodb://localhost/yourDB');
+/* mongoose.connect("mongodb://localhost/Weather") */
